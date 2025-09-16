85e3b2194bf012711dfef56637b42bf4903180ef
plarke0 <plarke@byu.edu>
2025-09-16 16:34:31 -0600

    Update deployFiles.sh

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-while getopts k:h:s: flag
+while getopts s: flag
 do
     case "${flag}" in
-        k) key=${OPTARG};;
-        h) hostname=${OPTARG};;
         s) service=${OPTARG};;
     esac
 done
 
-if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
+if [[ -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
+    printf "  syntax: deployFiles.sh -s <service>\n\n"
     exit 1
 fi
 
-printf "\n----> Deploying files for $service to $hostname with $key\n"
+printf "\n----> Deploying files for $service to server\n"
 
 # Step 1
 printf "\n----> Clear out the previous distribution on the target.\n"
-ssh -i "$key" ubuntu@$hostname << ENDSSH
+ssh web << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
 printf "\n----> Copy the distribution package to the target.\n"
-scp -r -i "$key" * ubuntu@$hostname:services/$service/public
+scp -r * web:services/$service/public
