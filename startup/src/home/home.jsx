35f4e661564c4dfeb5b64f7e00fde00ea5107134
plarke0 <plarke@byu.edu>
2025-10-08 22:39:47 -0600

    Add home body

--- startup/src/home/home.jsx
+++ startup/src/home/home.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export function Home() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Home displayed here</div>
+    <main>
+        <div class="container d-flex flex-column align-content-normal">
+            <div class="d-flex row row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-start g-2">
+                <NavLink to="login">
+                    <img src="./temp.jpg" alt="Budget Central" class="img-fluid shadow"/>
+                </NavLink>
+                <NavLink to="login">
+                    <img src="./temp.jpg" alt="Budget Central" class="img-fluid shadow"/>
+                </NavLink>
+                <NavLink to="login">
+                    <img src="./temp.jpg" alt="Budget Central" class="img-fluid shadow"/>
+                </NavLink>
+                <NavLink to="login">
+                    <img src="./temp.jpg" alt="Budget Central" class="img-fluid shadow"/>
+                </NavLink>
+                <NavLink to="login">
+                    <img src="./temp.jpg" alt="Budget Central" class="img-fluid shadow"/>
+                </NavLink>
+            </div>
+        </div>
     </main>
   );
 }
