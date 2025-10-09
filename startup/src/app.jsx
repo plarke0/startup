d3ac2f4bbebc535825c278260d60c3fc7d115ca4
plarke0 <plarke@byu.edu>
2025-10-08 21:52:14 -0600

    Add basic framework to app and index

---
+++ startup/src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return <div className="body bg-dark text-light">App will display here</div>;
+}
