d628253af100acbb415e82ed3a573717dcb3f92e
plarke0 <plarke@byu.edu>
2025-10-19 22:30:49 -0600

    Generalize about section entries
    Add additional section

--- startup/src/about/about.jsx
+++ startup/src/about/about.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+function AboutSection({ title, content }) {
+    return (
+        <div className="d-flex flex-column mb-2">
+            <span><b>{title}</b></span>
+            <span>{content}</span>
+        </div>
+    );
+}
+
 export function About() {
     return (
         <main>
@@ -8,18 +17,22 @@ export function About() {
                     <h2>About:</h2>
                 </div>
                 <div className="d-flex flex-column">
-                    <div className="d-flex flex-column mb-2">
-                        <span><b>What is this site?</b></span>
-                        <span>This is a site where I post my various web projects! I only have one so far, but I hope to add more as time goes on.</span>
-                    </div>
-                    <div className="d-flex flex-column mb-2">
-                        <span><b>What is Budget Center?</b></span>
-                        <span>Budget Center is the result of me wanting to make my own budgeting application. This is about the third iteration, and made a good project for my CS 260 class.</span>
-                    </div>
-                    <div className="d-flex flex-column mb-2">
-                        <span><b>What are my plans for this site?</b></span>
-                        <span>I'm planning to continue adding projects to this site. What those projects are remains to be seen. Really it will be anything that I think would be interesting.</span>
-                    </div>
+                    <AboutSection
+                        title="What is this site?"
+                        content="This is a site where I post my various web projects! I only have one so far, but I hope to add more as time goes on."
+                    />
+                    <AboutSection
+                        title="What is Budget Center?"
+                        content="Budget Center is the result of me wanting to make my own budgeting application. This is about the third iteration, and made a good project for my CS 260 class."
+                    />
+                    <AboutSection
+                        title="What are my plans for this site?"
+                        content="I'm planning to continue adding projects to this site. What those projects are remains to be seen. Really it will be anything that I think would be interesting."
+                    />
+                    <AboutSection
+                        title="Do you have any projects in mind?"
+                        content="I do actually! I recently had the idea to make an online version of scripture baseball, if you've ever heard of that. Then you can play and practice by yourself. Then you can whip out your skills when the Elders come over! I also want to make an online grade/GPA calculator and a clean music filter, but no promises on any of these, especially with my current workload from school."
+                    />
                 </div>
             </div>
         </main>
