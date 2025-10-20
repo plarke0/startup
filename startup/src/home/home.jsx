237ae2d2da732442c35b6a80fa0d5b8f9d13af05
plarke0 <plarke@byu.edu>
2025-10-19 22:20:31 -0600

    Generalize image links on home page

--- startup/src/home/home.jsx
+++ startup/src/home/home.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-//TODO: Make an image link component
+function ImageLink({ src, alt, destination }) {
+    return(
+        <Link to={destination}>
+            <img src={src} alt={alt} className="img-fluid shadow"/>
+        </Link>
+    );
+}
 
 export function Home() {
     return (
         <main>
             <div className="container d-flex flex-column align-content-normal">
                 <div className="d-flex row row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-start g-2">
-                    <NavLink to="login">
-                        <img src="./temp.jpg" alt="Budget Central" className="img-fluid shadow"/>
-                    </NavLink>
-                    <NavLink to="login">
-                        <img src="./temp.jpg" alt="Budget Central" className="img-fluid shadow"/>
-                    </NavLink>
-                    <NavLink to="login">
-                        <img src="./temp.jpg" alt="Budget Central" className="img-fluid shadow"/>
-                    </NavLink>
-                    <NavLink to="login">
-                        <img src="./temp.jpg" alt="Budget Central" className="img-fluid shadow"/>
-                    </NavLink>
-                    <NavLink to="login">
-                        <img src="./temp.jpg" alt="Budget Central" className="img-fluid shadow"/>
-                    </NavLink>
+                    <ImageLink src="./temp.jpg" alt="Budget Central" destination="login"/>
+                    <ImageLink src="./temp.jpg" alt="Temp" destination="temp"/>
+                    <ImageLink src="./temp.jpg" alt="Temp" destination="temp"/>
+                    <ImageLink src="./temp.jpg" alt="Temp" destination="temp"/>
+                    <ImageLink src="./temp.jpg" alt="Temp" destination="temp"/>
                 </div>
             </div>
         </main>
