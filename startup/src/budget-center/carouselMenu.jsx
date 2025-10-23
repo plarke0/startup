57c1a91650bb9b98008e065cec3362d60a871ca3
plarke0 <plarke@byu.edu>
2025-10-22 18:58:38 -0600

    Mock up carousel menu

--- startup/src/budget-center/carouselMenu.jsx
+++ startup/src/budget-center/carouselMenu.jsx
@@ -1,1 +1,21 @@
 import React from "react";
+import { Button } from 'react-bootstrap';
+
+export default function CarouselMenu({ carouselName, children }) {
+    return (
+        <div>
+            {/* Carousel Controls */}
+            <div className="card-header text-center">
+                <Button variant="outline-dark" className="me-1" data-bs-target={`#${carouselName}-carousel`} data-bs-slide="previous">&lt;</Button>
+                <label>Control Type</label>
+                <Button variant="outline-dark" className="ms-1" data-bs-target={`#${carouselName}-carousel`} data-bs-slide="next">&gt;</Button>
+            </div>
+            {/* Carousel Body */}
+            <div id={`${carouselName}-carousel`} className="carousel carousel-dark-slide" data-bs-ride="false">
+                <div className="carousel-inner">
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
