09ae5502cf95861191483b0910398fa01fa55e73
plarke0 <plarke@byu.edu>
2025-11-04 16:45:26 -0700

    Add endpoint structure to index.js

--- startup/service/index.js
+++ startup/service/index.js
@@ -1,3 +1,64 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
+
+const authCookieName = 'token';
+
+let users = [];
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use(express.static('public'));
+
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+//TODO
+//Create new user
+
+
+//TODO
+//Login exisiting user
+
+
+//TODO
+//Logout current user
+
+
+//TODO
+//Middleware to verify that the user is authenticated
+
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+
+
+
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
