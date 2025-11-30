409fa3cb4915deeb5111d88430314a8c9f0194eb
plarke0 <plarke@byu.edu>
2025-11-29 21:02:03 -0700

    Outline svg chart functionality

--- startup/src/budget-center/svgChart.jsx
+++ startup/src/budget-center/svgChart.jsx
@@ -3,7 +3,23 @@ import React, { useState } from "react";
 //TODO: make functional and animate
 //*https://stackoverflow.com/questions/59921845/math-to-create-svg-pie-chart-without-using-css-or-js
 
-export default function SVGChart({ total }) {
+export default function SVGChart({ categoryValues, total }) {
+    const strokeWidth = 30;
+    const svgWidth = 230;
+    const svgHeight = 230;
+    const chartRadius = 115;
+    const colorPalette = [
+        "#3EB680",
+        "#8ED8B7",
+        "#6B8CEF",
+        "#69B8F4",
+        "#C899F4",
+        "#BECFF6",
+        "#DEBA02",
+        "#FDE35A",
+        "#B6BEC7"
+    ];
+
     function stringifyMoney(value) {
         const valueString = (Math.abs(value)/100).toFixed(2);
         if (value < 0) {
@@ -13,10 +29,17 @@ export default function SVGChart({ total }) {
         }
     }
 
+    function generateArcData(arcNumber, arcPercentage, elapsedPercentage) {
+
+    }
+
     return (
-        <svg width="230" height="230">
-            <circle cx="110" cy="110" r="80" stroke="green" strokeWidth="40" fill="#ffffff"/>
-            <text fill="#000000" fontSize="40" justify="center" fontFamily="Monospace" x="40" y="125">{stringifyMoney(total)}</text>
+        <svg width={svgWidth} height={svgHeight}>
+            <path
+                fill="none" stroke={colorPalette[0]} strokeWidth={strokeWidth}
+                d="M 115 115 A 90 90 0 1 1 -57.85088487178855 -68.943999880708"
+            ></path>
+            <text fill="#000000" fontSize="40" fontFamily="Monospace" x="50%" y="50%" dominant-baseline="middle" text-anchor="middle">{stringifyMoney(total)}</text>
         </svg>
     );
 }
