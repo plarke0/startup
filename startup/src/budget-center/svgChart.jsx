a5cf0747d1e9aae6cbde3278d679999a5b7150d2
plarke0 <plarke@byu.edu>
2025-11-29 21:34:30 -0700

    Begin arc functions for chart

--- startup/src/budget-center/svgChart.jsx
+++ startup/src/budget-center/svgChart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //TODO: make functional and animate
 //*https://stackoverflow.com/questions/59921845/math-to-create-svg-pie-chart-without-using-css-or-js
@@ -20,6 +20,14 @@ export default function SVGChart({ categoryValues, total }) {
         "#B6BEC7"
     ];
 
+    const [chartArcs, setChartArcs] = useState([]);
+
+    useEffect(() => {
+        if (categoryValues && typeof categoryValues === "object") {
+            setChartArcs(getChartArcs());
+        }
+    }, [categoryValues]);
+
     function stringifyMoney(value) {
         const valueString = (Math.abs(value)/100).toFixed(2);
         if (value < 0) {
@@ -29,12 +37,36 @@ export default function SVGChart({ categoryValues, total }) {
         }
     }
 
-    function generateArcData(arcNumber, arcPercentage, elapsedPercentage) {
+    function getChartArcs() {
+        const valuesList = Object.entries(categoryValues).sort((a, b) => b[1] - a[1]);
+        let arcList = [];
+        let arcNumber = 0;
+        let elapsedPercentage = 0;
+        for (const valueDatum of valuesList) {
+            const value = valueDatum[1];
+            if (value <= 0) {
+                continue
+            }
+            const arcPercent = value / total;
+            arcList.push(generateArcData(arcNumber, arcPercent, elapsedPercentage));
+            arcNumber ++;
+            elapsedPercentage += arcPercent;
+        }
+        return arcList;
+    }
 
+    function generateArcData(arcNumber, arcPercentage, elapsedPercentage) {
+        
     }
 
     return (
         <svg width={svgWidth} height={svgHeight}>
+            {chartArcs.map((arc) => 
+                <path
+                    fill="none" stroke={arc.color} strokeWidth={strokeWidth}
+                    d={`M ${arc.startX} ${arc.startY} A ${chartRadius} ${chartRadius} 0 ${arc.isLargeArc} ${arc.isLargeSweep} ${arc.endX} ${arc.endY}`}
+                ></path>
+            )}
             <path
                 fill="none" stroke={colorPalette[0]} strokeWidth={strokeWidth}
                 d="M 115 115 A 90 90 0 1 1 -57.85088487178855 -68.943999880708"
